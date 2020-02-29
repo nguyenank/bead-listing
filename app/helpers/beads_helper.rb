df98bb80e03ae1a89afebb975879505bd6d1d3b5
An <anknguyenanknguyen@gmail.com>
2020-02-28 22:21:29 -0800

    create bead index layout

--- app/helpers/beads_helper.rb
+++ app/helpers/beads_helper.rb
@@ -1,15 +1,15 @@
 module BeadsHelper
 
-  def bead_list(beads)
-    string = ""
-    beads.each do |bead|
-      string += bead.brand
-      string += bead.color
-      string += bead.size
-      string += bead.shape
+def bead_list()
+  content_tag :div, class: "bead-list" do
+    Bead.order(updated_at: :desc).collect do |bead|
+        concat(bead_link(bead))
     end
-    return string
+    3.times do
+      concat(content_tag :div, "", class: "bead-placeholder")
+    end
   end
+end
 
 # https://stackoverflow.com/a/50450751
 # https://stackoverflow.com/a/4674091
@@ -22,6 +22,31 @@ def dropdown_generate(column_name)
   end
 end
 
+def each_bead(bead)
+  content_tag :div, class: "bead", id:bead.id do
+    concat(content_tag(:p,
+          (content_tag(:span, "Brand", class: "underline"))\
+          +(content_tag(:span, ": "))+ bead.brand))
+    concat(content_tag(:p,
+          (content_tag(:span, "Color", class: "underline"))\
+          +(content_tag(:span, ": "))+ bead.color))
+    concat(content_tag(:p,
+          (content_tag(:span, "Size", class: "underline"))\
+          +(content_tag(:span, ": "))+ bead.size))
+    concat(content_tag(:p,
+          (content_tag(:span, "Shape", class: "underline"))\
+          +(content_tag(:span, ": "))+ bead.shape))
+    concat(link_to("Edit Bead", edit_bead_path(bead.id), class: "bead-link" ))
+  end
+end
+
+private
+
+  def bead_link(bead)
+    link_to bead_path(bead.id) do
+      concat(each_bead(bead))
+    end
+  end
 
 
 end
