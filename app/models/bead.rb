0c1251fccd15fadc500b8d5ca220e3108e00b0fa
An <anknguyenanknguyen@gmail.com>
2020-03-01 20:26:25 -0800

    add error messages for invalid inputs

--- app/models/bead.rb
+++ app/models/bead.rb
@@ -1,6 +1,7 @@
 class Bead < ApplicationRecord
   validates :brand, presence: true
-  validates :color, presence: true
-  validates :size, presence: true
-  validates :shape, presence: true
+  validates :color, presence: true, length: {maximum: 32}
+  validates :size, presence: true, length: {maximum: 32}
+  validates :shape, presence: true, length: {maximum: 32}
+  validates :description, length: {maximum: 100000}
 end
